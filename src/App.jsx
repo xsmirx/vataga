a559527aff0e83abeedafd4c0624015abe451789
Ivan S <xsmirx@gmail.com>
2023-08-13 13:26:43 +0400

    test image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
 
 function App() {
-  return <div>Hello Vataga</div>
+  return (
+    <div>
+      <p>Hello Vataga</p>
+      <img src="https://raw.githubusercontent.com/devope/album/main/1.png" alt="image" />
+    </div>
+  )
 }
 
 export default App
