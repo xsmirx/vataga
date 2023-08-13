98ee45dcacbe58d7b30899ed8949887a842f8faf
DinaraKnyazeva <Dinarka470@mail.ru>
2023-08-13 16:31:42 +0400

    added filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import {useEffect, useState} from 'react'
-import {Header} from './components/Header'
-import {Filter} from './components/Filter'
-import CardGame from './components/cardGame/CardGame'
-import dataJson from './components/cardGame/done.json'
+import { useEffect, useState } from "react";
+import { Header } from "./components/Header";
+import { Filter } from "./components/Filter";
+import CardGame from "./components/cardGame/CardGame";
+import dataJson from "./components/cardGame/done.json";
 
 function App() {
-  const [data, setData] = useState()
-  const [findValue, setFindValue] = useState('')
-  const [time, setTime] = useState()
-  const [difficulty, setDifficulty] = useState()
-  const [players, setPlayers] = useState()
+  const [data, setData] = useState();
+  const [findValue, setFindValue] = useState("");
+  const [time, setTime] = useState();
+  const [difficulty, setDifficulty] = useState();
+  const [players, setPlayers] = useState();
 
   useEffect(() => {
-    setData(dataJson)
-  }, [])
+    setData(dataJson);
+  }, []);
 
   const handleChangeFind = (e) => {
-    setFindValue(e.target.value)
-  }
+    setFindValue(e.target.value);
+  };
 
   const handleTime = (e) => {
-    setTime(e.target.value)
-  }
+    setTime(e.target.value);
+  };
 
   const handleDifficulty = (e) => {
-    setDifficulty(e.target.value)
-  }
+    setDifficulty(e.target.value);
+  };
 
   const handlePlayers = (e) => {
-    setPlayers(e.target.value)
-  }
+    setPlayers(e.target.value);
+  };
 
   return (
     <div className="container m-auto px-4">
@@ -46,11 +46,35 @@ function App() {
         {data &&
           data
             .filter((item) => {
-              return item
+              return item.title.toLowerCase().includes(findValue.toLowerCase());
+            })
+            .filter((item) => {
+              if (time === undefined) {
+                return item;
+              } else if (time <= item.maxTime && time >= item.minTime) {
+                return item;
+              }
+            })
+            .filter((item) => {
+              if (players === undefined) {
+                return item;
+              } else if (
+                players <= item.maxPlayers &&
+                players >= item.minPlayer
+              ) {
+                return item;
+              }
+            })
+            .filter((item) => {
+              console.log(typeof difficulty, typeof item.difficulty);
+              if (difficulty === undefined) {
+                return item;
+              } else if (Number(difficulty) === item.difficulty) return item;
             })
             .map((item) => {
               return (
                 <CardGame
+                  key={item.title}
                   src={item.src[0]}
                   title={item.title}
                   description={item.description}
@@ -58,12 +82,13 @@ function App() {
                   minTime={item.minTime}
                   maxPlayers={item.maxPlayers}
                   minPlayer={item.minPlayer}
+                  difficulty={item.difficulty}
                 />
-              )
+              );
             })}
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
